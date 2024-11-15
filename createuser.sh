731475b4eae1b53bcb047fde1848bcc7d8fd72d7
Jagoum-Richmond <Jagoum>
2024-11-15 13:36:47 +0100

    Added a file to /etc/skel/ dir for a new user.

--- createuser.sh
+++ createuser.sh
@@ -1,9 +1,10 @@
 #/bin/bash
-	sudo usermod -aG sudo $user
+
 echo -e "Hello and Welcome To Creating user account withhout stress\nEnter the user name "
 read user
 echo -e "\n\n\t Please Kindly fill the following infos Below \n\n"
 
+	sudo usermod -aG sudo $user
 sudo adduser $user
 num=$(echo $?)
 	echo -e "\n\n\aUser $user successfully added \n\a\n"
